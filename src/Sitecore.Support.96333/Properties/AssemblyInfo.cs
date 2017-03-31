e52d09eb61c2bcb3272a065f1e565ab834a530bc
DK\psk <psk@sitecore.net>
2017-03-31 15:59:44 -0400

    Patch is done

---
+++ src/Sitecore.Support.96333/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.96333")]
+[assembly: AssemblyProduct("Sitecore.Support.96333")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Wrong search index might be used when searching in the Media Library")]
+
