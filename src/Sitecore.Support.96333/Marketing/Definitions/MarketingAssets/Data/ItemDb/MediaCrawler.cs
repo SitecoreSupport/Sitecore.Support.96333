08729ca9d1edcb58ff9413e02be86c7875355849
DK\hly <hly@sitecore.net>
2018-10-23 11:25:01 -0400

    Wrong search index might be used when searching in the Media Library

---
+++ src/Sitecore.Support.96333/Marketing/Definitions/MarketingAssets/Data/ItemDb/MediaCrawler.cs
@@ -0,0 +1,11 @@
+﻿namespace Sitecore.Support.Marketing.Definitions.MarketingAssets.Data.ItemDb
+{
+  using ContentSearch;
+  public class MediaCrawler : Sitecore.Marketing.Definitions.MarketingAssets.Data.ItemDb.MediaCrawler
+  {
+    public override int GetContextIndexRanking(IIndexable indexable)
+    {
+      return int.MaxValue;
+    }
+  }
+}
